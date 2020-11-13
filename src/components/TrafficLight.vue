bd6800b00c21a0e223527992407148acced8c1ee
Alan Reid <alanreidt@gmail.com>
2020-11-13 11:41:58 +0300

    Remove the css animation (closes #36)

--- src/components/TrafficLight.vue
+++ src/components/TrafficLight.vue
@@ -3,7 +3,7 @@
     <div class="protector"></div>
     <div class="protector protector_middle"></div>
     <div class="protector protector_bottom"></div>
-    <div class="light"></div>
+    <div class="light light_red light_active"></div>
     <div class="light light_yellow"></div>
     <div class="light light_green"></div>
   </div>
@@ -55,58 +55,26 @@ export default defineComponent({
 }
 
 .light {
-  margin: 16px;
   width: 100px;
   height: 100px;
+  margin: 16px;
 
   background: repeating-linear-gradient(#333, #443 5px);
   border-radius: 50%;
-
-  animation-name: lightUpRed;
-  animation-duration: 4s;
-  animation-iteration-count: infinite;
-  animation-timing-function: step-end;
 }
 
-.light_yellow {
-  animation-name: lightUpYellow;
+.light_active.light_red {
+  background: repeating-linear-gradient(#f00, #e00 5px);
+  box-shadow: 0 0 40px #f00;
 }
 
-.light_green {
-  animation-name: lightUpGreen;
+.light_active.light_yellow {
+  background: repeating-linear-gradient(#fd0, #ec0 5px);
+  box-shadow: 0 0 40px #fd0;
 }
 
-@keyframes lightUpRed {
-  0% {
-    background: repeating-linear-gradient(#f00, #e00 5px);
-    box-shadow: 0 0 40px #f00;
-    z-index: 1;
-  }
-  35% {
-    background: repeating-linear-gradient(#443, #333 5px);
-    box-shadow: none;
-    z-index: 0;
-  }
-}
-
-@keyframes lightUpYellow {
-  80% {
-    background: repeating-linear-gradient(#fd0, #ec0 5px);
-    box-shadow: 0 0 40px #fd0;
-    z-index: 1;
-  }
-}
-
-@keyframes lightUpGreen {
-  35% {
-    background: repeating-linear-gradient(#0d0, #0c0 5px);
-    box-shadow: 0 0 40px #0d0;
-    z-index: 1;
-  }
-  80% {
-    background: repeating-linear-gradient(#443, #333 5px);
-    box-shadow: none;
-    z-index: 0;
-  }
+.light_active.light_green {
+  background: repeating-linear-gradient(#0d0, #0c0 5px);
+  box-shadow: 0 0 40px #0d0;
 }
 </style>
