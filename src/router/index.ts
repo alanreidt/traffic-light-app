91fa7fcb2b671fab1a730ccb8d933dd8ecf65953
Alan Reid <alanreidt@gmail.com>
2020-11-15 18:27:54 +0300

    Create a route for each lens (closes #35)

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,11 @@ const routes: Array<RouteRecordRaw> = [
     name: "Home",
     component: Home
   },
+  {
+    path: "/:activeLight",
+    name: "HomeWithActiveLight",
+    component: Home
+  },
 ];
 
 const router = createRouter({
