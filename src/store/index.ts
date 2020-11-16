1b7f140b7231ff74e7c007471fd6e6a0462e5d47
Alan Reid <alanreidt@gmail.com>
2020-11-16 11:14:07 +0300

    Create actions (closes #72)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { createStore } from "vuex";
-import { Color, TimerId } from "@/constants";
+import { Color, TimerId, MILISECONDS_IN_A_SECOND } from "@/constants";
 
 export default createStore({
   state: {
@@ -9,5 +9,21 @@ export default createStore({
   },
   getters: {},
   mutations: {},
-  actions: {},
+  actions: {
+    changeActiveLight({ commit }: any) {
+      const activeLight = "red"; // calc active light here
+
+      commit("changeActiveLight", activeLight);
+    },
+    startTimer({ commit }: any) {
+      const timerId = setInterval(() => {
+        commit("updateTimer");
+      }, MILISECONDS_IN_A_SECOND);
+
+      commit("setTimer", timerId);
+    },
+    resetTimer({ commit }: any) {
+      commit("resetTimer");
+    },
+  },
 });
