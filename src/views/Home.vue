b7ad257b661185c760e24bb8f910217d84dfd028
Alan Reid <alanreidt@gmail.com>
2020-11-17 20:54:17 +0300

    Configure reaction on the route change via the history api (closes #91)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { mapActions, mapState } from "vuex";
+import { RouteParams } from "vue-router";
 
 import TrafficSignal from "@/components/TrafficSignal.vue";
 
@@ -27,6 +28,13 @@ export default defineComponent({
     const activeLight = this.$route.params.activeLight;
 
     this.setActiveLight(activeLight);
+
+    this.$watch(
+      () => this.$route.params,
+      ({ activeLight }: RouteParams) => {
+        this.setActiveLight(activeLight);
+      }
+    );
   },
   components: {
     TrafficSignal
